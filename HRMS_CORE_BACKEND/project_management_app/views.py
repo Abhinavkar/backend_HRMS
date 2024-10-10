853d0e496a6a78ac79eb88aed8e88a6f58c4f6d4
Abhinav kar <abhinav.kar@vvdntech.in>
2024-10-10 09:36:32 +0530

    Added_model_and_views_for_home

--- HRMS_CORE_BACKEND/project_management_app/views.py
+++ HRMS_CORE_BACKEND/project_management_app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.shortcuts import redirect
+# from .models import User
 # Create your views here.
+def home_page(request):
+    return HttpResponse("HI")
+
+
+
+def super_user_login_page(request):
+    if request.user.is_authenticated :
+        redirect('home')
