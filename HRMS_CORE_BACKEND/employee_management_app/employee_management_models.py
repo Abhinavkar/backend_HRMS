28fdec5f6a83da52fc7fd1d56be963e612e6114b
diganta mohanty <diganta.mohanty@vvdntech.in>
2024-10-16 11:47:15 +0530

    Added Template

--- HRMS_CORE_BACKEND/employee_management_app/employee_management_models.py
+++ HRMS_CORE_BACKEND/employee_management_app/employee_management_models.py
@@ -20,11 +20,11 @@ class Skill(models.Model):
 
 # TechStack model
 class TechStack(models.Model):
-    techstack_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    techstack_name = models.CharField(max_length=100, verbose_name="Tech Stack Name")
+    TechStack_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    TechStack_name = models.CharField(max_length=100, verbose_name="Tech Stack Name")
 
     def __str__(self):
-        return self.techstack_name
+        return self.TechStack_name
 
 # Engagement model
 class Engagement(models.Model):
