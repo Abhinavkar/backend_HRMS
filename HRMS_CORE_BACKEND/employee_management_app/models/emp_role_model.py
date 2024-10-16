6dfc53d862c0c6bbed21c7ec0d57452a4710c797
Abhinav kar <abhinav.kar@vvdntech.in>
2024-10-16 12:09:26 +0530

    Updated Models

--- HRMS_CORE_BACKEND/employee_management_app/models/emp_role_model.py
+++ HRMS_CORE_BACKEND/employee_management_app/models/emp_role_model.py
@@ -1,11 +1,5 @@
 from django.db import models
-import uuid
 
-class EmployeeAndProject(models.Model):
+class EmployeeRoleRelation(models.Model):
     employee = models.ForeignKey('EmployeeData', on_delete=models.CASCADE)
-    project = models.ForeignKey('Project', on_delete=models.CASCADE)
-    type_of_engagement = models.ForeignKey('Engagement', on_delete=models.SET_NULL, null=True)
-    percentage_allocation = models.FloatField()
-    role = models.CharField(max_length=100)
-    allocation_date = models.DateTimeField()
-    deallocation_date = models.DateTimeField(null=True, blank=True)
+    role = models.ForeignKey('Role', on_delete=models.CASCADE)
