32ecf564132a06d74dad4bbd6da5c3ed207ba3be
diganta mohanty <diganta.mohanty@vvdntech.in>
2024-10-25 17:07:42 +0530

    Added Techstack API

--- HRMS_CORE_BACKEND/HRMS_CORE_BACKEND/settings.py
+++ HRMS_CORE_BACKEND/HRMS_CORE_BACKEND/settings.py
@@ -81,25 +81,25 @@ WSGI_APPLICATION = 'HRMS_CORE_BACKEND.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    "default":{
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db_hrm',
-        'USER': 'postgres',
-        'PASSWORD': '123123',
-        'HOST': 'localhost',
-        'PORT': '5432',
-
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-
 }
+#
+# DATABASES = {
+#     "default":{
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'db_hrm',
+#         'USER': 'postgres',
+#         'PASSWORD': '123123',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#
+#     }
+#
+# }
 
 
 # Password validation
