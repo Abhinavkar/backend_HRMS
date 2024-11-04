b61d3e728b053ffa72d28b85c739101fd7a04fdc
diganta mohanty <diganta.mohanty@vvdntech.in>
2024-11-04 18:32:16 +0530

    Added API for create-project

--- HRMS_CORE_BACKEND/project_management_app/project_management_app_serializers.py
+++ HRMS_CORE_BACKEND/project_management_app/project_management_app_serializers.py
@@ -1,11 +1,16 @@
-# project_management_app_serializers.py
-
+from employee_management_app.models.bu_model import BusinessUnit as BusinessUnit
 from rest_framework import serializers
-from .project_management_app_models import Project
-from employee_management_app.employee_management_serializer import BusinessUnitSerializer
+from .project_management_app_models import Project # Adjust the import as needed
 
 class ProjectSerializer(serializers.ModelSerializer):
-    proj_bu_serializer = BusinessUnitSerializer()
+    # Use a PrimaryKeyRelatedField for the business unit
+    proj_business_unit = serializers.PrimaryKeyRelatedField(queryset=BusinessUnit.objects.all())
+
     class Meta:
         model = Project
-        fields = '__all__'
+        fields = '__all__'  # This now includes proj_business_unit directly
+
+    def create(self, validated_data):
+        # Create the Project instance
+        project = Project.objects.create(**validated_data)
+        return project
