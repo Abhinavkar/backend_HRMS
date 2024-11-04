a3917e17f4fc6f4d472038df6fec14831e7f70fe
Abhinav kar <abhinav.kar@vvdntech.in>
2024-11-04 09:53:16 +0530

    AddedEmailUtils

---
+++ HRMS_CORE_BACKEND/employee_management_app/utils/utils.py
@@ -0,0 +1,22 @@
+# Updated utils.py
+
+from django.core.mail import send_mail
+from django.conf import settings
+from django.core.mail import BadHeaderError
+from django.core.exceptions import SuspiciousOperation
+
+def send_hr_email(subject, message, recipient_list):
+    try:
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            recipient_list,
+            fail_silently=False,
+        )
+    except BadHeaderError:
+        # Handle the case where an invalid header was provided
+        raise SuspiciousOperation("Invalid header found.")
+    except Exception as e:
+        # Handle other potential errors
+        print(f"Error sending email: {e}")
