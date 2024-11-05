810e9793c2699ec497227895cc0b76799e1a3b6d
diganta mohanty <diganta.mohanty@vvdntech.in>
2024-11-05 12:17:07 +0530

    Added Create a class and define post,get, put API

--- HRMS_CORE_BACKEND/project_management_app/project_management_app_views.py
+++ HRMS_CORE_BACKEND/project_management_app/project_management_app_views.py
@@ -5,6 +5,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from .project_management_app_serializers import ProjectSerializer
+from .project_management_app_models import Project
+
+
 
 class ProjectManagementViewSet(APIView):
     permission_classes = [IsAuthenticated]
@@ -18,3 +21,27 @@ class ProjectManagementViewSet(APIView):
 
 
 ######################################### GET ################################################
+    def get(self, request):
+        projects = Project.objects.all() # Fetch all the projects from the database
+        serializer = ProjectSerializer(projects, many=True) # Serialize the projects using the ProjectSerializer
+        return Response(serializer.data, status=status.HTTP_200_OK) #Return the serialized data in the response with status 200
+
+####################################### PUT/PATCH ############################################
+
+    def put(self, request, pk):
+        try:
+            project = Project.objects.get(pk=pk)
+        except Project.DoesNotExist:
+            return Response({'detail': 'Project not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = ProjectSerializer(project, data=request.data, partial=False)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
